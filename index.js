2cd4c73e03470e3ade50fd578d37e803ed10c4a9
RohanK22 <rohank2205@gmail.com>
2021-01-26 22:05:43 +0530

    Added some server code

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+
+app.get('/ping', (req, res) => {
+    console.log('got a /ping request');
+    res.json({
+        message: 'Pong'
+    });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+let port = process.env.PORT || 9000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
